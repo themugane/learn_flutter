002932277d3aea584e38859428cb77272b4c43d5
themugane <themugane@gmail.com>
2022-09-08 22:13:02 +0300

    add custom font

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,8 @@ void main() => runApp(MaterialApp(
           fontSize: 20.0,
           fontWeight: FontWeight.bold,
           letterSpacing: 2.0,
-          color: Colors.grey[600]
+          color: Colors.grey[600],
+          fontFamily: 'IndieFlower',
         ),
       ),
     ),
