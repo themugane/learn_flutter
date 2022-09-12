1cc8d9db3a11ce273f209f915c4e12962d09031c
themugane <themugane@gmail.com>
2022-09-13 00:04:53 +0300

    Basic Template

--- lib/main.dart
+++ lib/main.dart
@@ -1,57 +1,16 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Home()
+  home: NinjaCard(),
 ));
 
-
-class Home extends StatelessWidget {
+class NinjaCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('my first app'),
-        centerTitle: true,
-        backgroundColor: Colors.red[600],
-      ),
-      body: Row(
-        children: <Widget>[
-          Expanded(
-            child: Image.asset('assets/stew.jpeg'),
-            flex: 3,
-          ),
-           Expanded(
-             flex: 3,
-             child: Container(
-               padding: EdgeInsets.all(30.0),
-               color: Colors.cyan,
-               child: Text('1')
-             ),
-           ),
-            Expanded(
-              flex: 2,
-              child: Container(
-                  padding: EdgeInsets.all(30.0),
-                  color: Colors.pinkAccent,
-                  child: Text('2')
-              ),
-            ),
-            Expanded(
-              flex: 1,
-              child: Container(
-                  padding: EdgeInsets.all(30.0),
-                  color: Colors.amber,
-                  child: Text('3')
-              ),
-            ),
-        ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Text('click'),
-        onPressed: () {},
-        backgroundColor: Colors.red[600],
+        title: Text('Ninja ID Card'),
       ),
     );
   }
 }
-
