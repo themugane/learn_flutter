a5b504263601d8415ecd6947daa28968d33a63ac
themugane <themugane@gmail.com>
2022-09-12 20:59:49 +0300

    row alignments

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,8 @@ class Home extends StatelessWidget {
         backgroundColor: Colors.red[600],
       ),
       body: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
           Text('hello, world'),
           TextButton(
