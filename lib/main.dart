993dc79152fdfdcf9f50031e3ef430bb3b648a21
themugane <themugane@gmail.com>
2022-09-15 01:17:19 +0300

    Extracting widgets

--- lib/main.dart
+++ lib/main.dart
@@ -20,32 +20,7 @@ class _QuoteListState extends State<QuoteList> {
   ];
 
   Widget quoteTemplate(quote) {
-    return Card(
-      margin: EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0.0),
-      child: Padding(
-        padding: const EdgeInsets.all(12.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Text(
-                quote.text,
-              style: TextStyle(
-                fontSize: 18.0,
-                color: Colors.grey[600],
-              ),
-            ),
-            SizedBox(height: 6.0),
-            Text(
-              quote.author,
-              style: TextStyle(
-                fontSize: 14.0,
-                color: Colors.grey[800]
-              ),
-            )
-          ],
-        ),
-      ),
-    );
+    return QuoteCard(quote: quote);
   }
 
   @override
@@ -60,6 +35,42 @@ class _QuoteListState extends State<QuoteList> {
       body: Column(
           children: quotes.map((quote) => quoteTemplate(quote)).toList(),
         ),
+    );
+  }
+}
+
+class QuoteCard extends StatelessWidget {
+
+  final Quote ? quote;
+  QuoteCard({ this.quote });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0.0),
+      child: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Text(
+                quote!.text,
+              style: TextStyle(
+                fontSize: 18.0,
+                color: Colors.grey[600],
+              ),
+            ),
+            SizedBox(height: 6.0),
+            Text(
+              quote!.author,
+              style: TextStyle(
+                fontSize: 14.0,
+                color: Colors.grey[800]
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/quote.dart
+++ lib/quote.dart
@@ -1,13 +1,11 @@
 class Quote {
 
-  String ? text;
-  String ? author;
+  String text = "";
+  String author = "";
 
   // Quote({String ? text, String ? author}){
   //   this.text = text;
   //   this.author = author;
   // }
-  Quote({ this.text, this.author });
+  Quote({ required this.text, required this.author });
 }
-
-Quote myquote = Quote(text: 'this is the quote text', author:'Oscar Wilde');
