8377d7a0549baa1e1783fbe5e06e2bd0f0512e50
themugane <themugane@gmail.com>
2022-09-15 11:43:27 +0300

    routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:myapp/pages/choose_location.dart';
 // import 'pages/home.dart';
 import 'package:myapp/pages/home.dart';
+import 'package:myapp/pages/loading.dart';
 
 void main() => runApp(MaterialApp(
-  home: Home(),
+  initialRoute: '/home',
+  routes: {
+    '/': (context) => Loading(),
+    '/home': (context) => Home(),
+    '/location': (context) => ChooseLocation()
+  },
 ));
