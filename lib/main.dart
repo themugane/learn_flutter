2c6e8d9b594f5fce174b7c23a92b9968b85f5ccf
themugane <themugane@gmail.com>
2022-09-15 15:34:42 +0300

    http packages

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ import 'package:myapp/pages/home.dart';
 import 'package:myapp/pages/loading.dart';
 
 void main() => runApp(MaterialApp(
-  initialRoute: '/home',
+  initialRoute: '/',
   routes: {
     '/': (context) => Loading(),
     '/home': (context) => Home(),
--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'dart:developer';
 
 class ChooseLocation extends StatefulWidget {
   const ChooseLocation({Key? key}) : super(key: key);
@@ -9,27 +8,6 @@ class ChooseLocation extends StatefulWidget {
 }
 
 class _ChooseLocationState extends State<ChooseLocation> {
-
-  void getData() async {
-
-  //  Simulate a network request
-    String username = await Future.delayed(Duration(seconds: 3), () {
-      return 'Yoshi';
-    });
-    //  Simulate a network request to get bio of the username
-    String bio = await Future.delayed(Duration(seconds: 2), () {
-      return 'Vegan, musician & chess master';
-    });
-
-    log('$username - $bio');
-
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    getData();
-  }
   
   @override
   Widget build(BuildContext context) {
